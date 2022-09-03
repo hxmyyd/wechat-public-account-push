1ac70a3bd524d915fe30cd452b370d899238c068
hxmyyd <112741672+hxmyyd@users.noreply.github.com>
2022-09-03 12:50:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ export const config = {
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
    *    id: "oji1s5vDDUTgiNZoCSgFpRwKWe3s",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "nw98tW5VWyW5dtDtlMkIu9Lysz_OlmaWOyDUrRuNsU4U",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oji1s5vDDUTgiNZoCSgFpRwKWe3s",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "zjVglIuC2Xo4CsraXWEio1iP7sZJ37IQEES9lD3sgc4yOU",
+      useTemplateId: "nw98tW5VWyW5dtDtlMkIu9Lysz_OlmaWOyDUrRuNsU4",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }
