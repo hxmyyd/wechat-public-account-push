a6249f540c846d16603aab70ae8a71671f18b193
hxmyyd <112741672+hxmyyd@users.noreply.github.com>
2022-09-03 15:18:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -9,21 +9,7 @@ export const config = {
   // 公众号APP_SECRET
   APP_SECRET: "6834bf6a4b2d843b565b363b39200e60",
 
-  /**
-   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-   * [
-   *  {
-   *    // 想要发送的人的名字
-   *    name: "淼淼",
-   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "oji1s5vDDUTgiNZoCSgFpRwKWe3s",
-   *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "nw98tW5VWyW5dtDtlMkIu9Lysz_OlmaWOyDUrRuNsU4U",
-   *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
-   *   }, 
-   * ]
-   */
+ 
   USERS: [
     {
       // 想要发送的人的名字
@@ -37,10 +23,6 @@ export const config = {
     }
   ],
 
-  /**
-   * 回调消息 相关，主要用来展示发送是否成功/失败的数据
-   */
-
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: "zjVglIuCXo34CsraXWEio1i3P7sZJ7IQEES93lDsgc4yOU",
 
@@ -53,26 +35,13 @@ export const config = {
       id: "oji1s5vDDUTgiNZoCSgFpRwKWe3s",
     }, 
   ],
-    
-  /**
-   * 信息配置
-   */
-
-  /** 天气相关 */
-
+ 
   // 所在省份
   PROVINCE: "河北",
   // 所在城市
   CITY: "衡水",
 
-  /** 重要节日相关 */
-
-  /**
-   * 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
-   * tpye必须填！ 只能 “生日” 和 “节日” 二选一!
-   * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
-   * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-   */
+  
   FESTIVALS: [
     {"type": "生日", "name": "淼淼", "year": "2004", "date": "01-01"},
     
@@ -80,11 +49,7 @@ export const config = {
     {"type": "节日", "name": "纪念日", "year": "2021", "date": "12-30"},
   ],
 
-  /**
-   * 限制重要节日的展示条目, 需要填写数字; 
-   * 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
-   * 如果为0, 则默认展示全部
-   */
+ 
   FESTIVALS_LIMIT: 3,
 
   /** 日期相关 */
@@ -93,11 +58,11 @@ export const config = {
     * keyword是指暴露给测试号的模板字段，填什么就暴露什么, 请注意不要和README的出参表中的字段重复。
     * 比如：keyword: "love_date" ，在测试号中就是 {{ love_date.DATA }} 
     * */
-  CUSTOMIZED_DATE_LIST: [
+ /* CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
     {"keyword": "love_day", date: "2021-12-30"},
     
-  ],
+  ],*/
 
   /** 插槽 */
 
@@ -107,14 +72,14 @@ export const config = {
     * */
   SLOT_LIST: [
     
-    // 这样配置的话，就会每次随机选一句话发送
+   /* // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
       "因为太喜欢你，所以看谁都像是情敌。",
       "申请成为你爱里的永久居民。",
       "你很傻，你很笨，可我还是很羡慕你，因为你有我",
       "遇见你，就好像捡到了100斤的运气",
     ]},
-  ],
+  ],*/
 
   /** 每日一言 */
 
