db3b45975a5a97eb7780918a0f2316a6a40bb90e
hxmyyd <112741672+hxmyyd@users.noreply.github.com>
2022-10-27 09:37:40 +0800

    Update and rename config/index.js to 配置/index.js

--- 配置/index.js
+++ 配置/index.js
@@ -4,10 +4,10 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "wx4b2e04b3d3772b55",
+  APP_ID:"wx4b2e04b3d3772b55",
 
   // 公众号APP_SECRET
-  APP_SECRET: "6834bf6a4b2d843b565b363b39200e60",
+  APP_SECRET:"6834bf6a4b2d843b565b363b39200e60",
 
  
   USERS: [
